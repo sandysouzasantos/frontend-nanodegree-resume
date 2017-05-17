8b4e302294645cb3417036fa1ca105a840e2f7c9
Sandy Santos <sandysouzasantos@gmail.com>
2017-05-17 10:04:22 -0300

    Fixes objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,43 +7,114 @@ var bio = {
         "github": "sandysouzasantos",
         "location": "Belo Horizonte, MG, Brazil"
     },
-    "picture": "images/me.jpeg",
-    "welcomeMessage": "Hello World!",
-    "skills": ["awesomeness", "programming", "JS"]
+    "biopic": "images/me.jpeg",
+    "welcomeMessage": "If you’d like to hire me, I’d love to hear from you.",
+    "skills": ["HTML5", "CSS3", "JS", "Bootstrap", "Foundation", "JQuery", "Grunt", "Web Semantics", "Responsivity", "Frontend Development", "Web Development"],
+    "display": function () {
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+        $("#header").prepend(formattedName + formattedRole);
+
+        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+        $("#topContacts").append(formattedEmail + formattedMobile + formattedGithub + formattedLocation);
+
+        var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+
+        var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        $("#header").append(formattedPic + formattedMsg);
+
+        if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+            for (skill in bio.skills) {
+                var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+                $("#skills").append(formattedSkill);
+            }
+        }
+    }
 };
 
 var work = {
     "jobs": [
         {
-            "position": "Web Developer",
+            "title": "Web Developer",
             "employer": "Freelancer",
-            "years": "2016 - Currently",
+            "dates": "2016 - In Progress",
             "location": "Belo Horizonte, MG, Brazil",
-            "description": "Frontend development. HTML5, CSS3, Bootstrap, Javascript, jQuery, Blade (Laravel)."
+            "description": "Frontend development. HTML5, CSS3, Bootstrap, Foundation, Javascript, jQuery, Grunt, Blade (Laravel)."
         },
         {
-            "position": "Lawyer",
-            "employer": "Freelancer, Escritório Rosi e Rajão e Ney Campos",
-            "years": "2015 - 2016",
+            "title": "Lawyer",
+            "employer": "Rosi e Rajão Advogados Associados, Ney Campos Advogados Associados, Freelancer",
+            "dates": "2015 - 2016",
             "location": "Belo Horizonte, MG, Brazil",
             "description": "Worked as a lawyer for various firms, drafting contracts, collection actions, labor and civil lawsuits, and intellectual property consulting."
         }
-    ]
+    ],
+    "display": function () {
+        for (job in work.jobs) {
+            $("#workExperience").append(HTMLworkStart);
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+            var formattedPosition = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+            var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+            var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+            $(".work-entry:last").append(formattedEmployer + formattedPosition);
+            $(".work-entry:last").append(formattedDates);
+            $(".work-entry:last").append(formattedDescription);
+        }
+    }
 };
 
 var education = {
     "schools": [
         {
-            "name": "Udacity",
-            "city": "Silicon Valley, CA, US",
-            "degree": "Nanodegree",
-            "major": ["Frontend Development", "HTML", "CSS", "JS"]
+            "name": "Universidade Federal de Viçosa",
+            "location": "Viçosa, MG, Brazil",
+            "degree": "BA",
+            "majors": ["Law School"],
+            "dates": "March, 2010 - December, 2014",
+            "url": ""
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Frontend Developer Nanodegree",
+            "school": "Udacity",
+            "dates": "In progress",
+            "url": "https://profiles.udacity.com/p/10395977147"
         },
         {
-            "name": "Universidade Federal de Viçosa",
-            "city": "Viçosa, MG, Brazil",
-            "degree": "BA",
-            "major": ["Law School"]
+            "title": "Interactivity with JavaScript",
+            "school": "Coursera",
+            "dates": "January, 2017",
+            "url": "https://www.coursera.org/account/accomplishments/verify/HKPD4GRYGK2H"
+        },
+        {
+            "title": "Advanced Styling with Responsive Design",
+            "school": "Coursera",
+            "dates": "January, 2017",
+            "url": "https://www.coursera.org/account/accomplishments/verify/38RVQSN2T2JX"
+        },
+        {
+            "title": "Introduction to CSS3",
+            "school": "Coursera",
+            "dates": "December, 2016",
+            "url": "https://www.coursera.org/account/accomplishments/verify/6WYL5LMSSUH8"
+        },
+        {
+            "title": "Introduction to HTML5",
+            "school": "Coursera",
+            "dates": "November, 2016",
+            "url": "https://www.coursera.org/account/accomplishments/verify/4G36WLXCVKNQ"
         }
     ]
 };
@@ -51,64 +122,33 @@ var education = {
 var projects = {
     "project": [
         {
-            "title": "ForSocios",
-            "url": "https://www.forsocios.com/",
-            "dates": "2016",
-            "images": "images/forsocios.jpg",
-            "description": ["HTML5", "CSS3"]
+            "title": "Crawly",
+            "url": "http://www.crawly.com.br/",
+            "dates": "January, 2017 - In progress",
+            "images": "",
+            "description": "Frontend development of private libraries for a company called Crawly."
         },
         {
             "title": "LinceApp",
             "url": "https://www.linceapp.com/",
-            "dates": "2016",
+            "dates": "February, 2017 - March, 2017",
             "images": "images/linceapp2.jpg",
-            "description": ["HTML5", "CSS3", "JS", "PHP", "Bootstrap"]
+            "description": "Frontend development. HTML5, CSS3, Bootstrap, Javascript, jQuery, Blade (Laravel)."
+        },
+        {
+            "title": "ForSocios",
+            "url": "https://www.forsocios.com/",
+            "dates": "December, 2016",
+            "images": "images/forsocios.jpg",
+            "description": "Frontend Development. HTML5, CSS3, Bootstrap"
         }
-
     ]
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
 
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+bio.display();
 
-$("#header").prepend(formattedName + formattedRole);
-
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedPic = HTMLbioPic.replace("%data%", bio.picture);
-var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-
-$("#topContacts").append(formattedEmail + formattedMobile + formattedGithub + formattedLocation);
-
-$("#header").append(formattedPic + formattedMsg);
-
-if (bio.skills.length > 0) {
-    $("#header").append(HTMLskillsStart);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-    $("#skills").append(formattedSkill);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-    $("#skills").append(formattedSkill);
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-    $("#skills").append(formattedSkill);
-}
-
-var displayWork = function () {
-    for (job in work.jobs) {
-        $("#workExperience").append(HTMLworkStart);
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedPosition = HTMLworkTitle.replace("%data%", work.jobs[job].position);
-        $(".work-entry:last").append(formattedEmployer + formattedPosition);
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].years);
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-        $(".work-entry:last").append(formattedDates);
-        $(".work-entry:last").append(formattedDescription);
-    }
-};
-
-displayWork();
+work.display();
 
 function locationizer(work_obj) {
     var locationArray = [];
@@ -117,7 +157,6 @@ function locationizer(work_obj) {
     }
     return locationArray;
 }
-
 // Did locationizer() work? This line will tell you!
 console.log(locationizer(work));
 
@@ -126,7 +165,7 @@ $("#main").append(internationalizeButton);
 var inName = function () {
     var finalName = bio.name;
     var name = bio.name.trim().split(" ");
-    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
     name[1] = name[1].toUpperCase();
     finalName = name.join(" ");
     return finalName;
@@ -134,7 +173,7 @@ var inName = function () {
 
 console.log(inName());
 
-//$().click(inName(bio));
+$("button").click(inName());
 
 projects.display = function () {
     for (proj in projects.project) {
